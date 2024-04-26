238c088f0cc1af84d706ca1ecb1c53a1a522fd36
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-04-26 11:33:32 +0530

    update code files and map

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made various projects in that language. I also occasionally do DSA problems in C++. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/SartHak-0-Sach" target="_blank">Github</a>! Please take out some time and check-out my github profile.`,
+  "degree": `This is my engineering degree. I hung it on the wall because I'm proud of it. It was in mechanical engineering but I'm a software engineer now.`,
+  "sofa-table": `That's my sofa. This is where I code and watch programming tutorials`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/SartHak-0-Sach/Resume/blob/main/Sarthak_Sachdev-resume.pdf" target="_blank">Check it out?</a>
+  Contact me at saarsaach30@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves.
+  I love to read programming books considering how much you can learn about the subject itself from them and that is when you realize that computer science is not just about learning how to code but much more.`,
+  exit: `If you want to exit Sarthak's portfolio, just close the tab.`,
+};
