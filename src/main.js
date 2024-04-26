6cb6c14528444d8d4150c1788e5f0389055d3b57
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-04-26 10:26:01 +0530

    add spritesheet and update code

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { k } from "./kaboomCtx";
+
+k.loadSprite("spritesheet", "./spritesheet.png", {
+    spriteX: 39,
+    spriteY: 31,
+    anims: {
+        "idle-down": 936,
+        "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
+        "idle-side": 975,
+        "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
+        "idle-up": 1014,
+        "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
+    },
+});
